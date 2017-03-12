48f82fac318d97327f671e2081f91b370b500c2d
Dave Rensin <dave@rensin.com>
2017-03-12 10:08:15 +0000

    playback tuning

--- infinite_jukebox.py
+++ infinite_jukebox.py
@@ -174,7 +174,7 @@ if __name__ == "__main__":
 
             how_long_this_took = display_playback_progress(v)
 
-            pygame.time.wait( int( (beat_to_play['duration'] - how_long_this_took) * 1000 ) )   
+            pygame.time.wait( int( (beat_to_play['duration'] - how_long_this_took) * 999 ) )   
                 
     except KeyboardInterrupt:
         print
