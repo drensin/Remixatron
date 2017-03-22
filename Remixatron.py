2dc10b0b07b63c3bde954371b05c435b20d4af45
Dave Rensin <dave@rensin.com>
2017-03-22 01:49:54 +0000

    play vector calculation bug fixup

--- Remixatron.py
+++ Remixatron.py
@@ -453,6 +453,8 @@ class InfiniteJukebox(object):
 
         recent = collections.deque(maxlen=recent_depth)
 
+        first_beat_with_candidates = next(b['id'] for b in beats if len(b['jump_candidates']) > 0)
+
         for i in range(0, 1024 * 1024):
 
             if beat['segment'] not in recent:
@@ -460,7 +462,7 @@ class InfiniteJukebox(object):
 
             current_sequence += 1
 
-            will_jump = (current_sequence == min_sequence)
+            will_jump = (current_sequence == min_sequence) or (beat == beats[-1])
 
             # if it's time to jump, then assign the next beat, and create
             # a new play sequence between 8 and 32 beats -- making sure
@@ -475,8 +477,11 @@ class InfiniteJukebox(object):
                 # earliest beat with ANY jump candidates (ie. the end of the song intro)
 
                 if len(non_recent_candidates) == 0:
-                    first_beat_with_candidates = next(b['id'] for b in beats if len(b['jump_candidates']) > 0)
-                    beat = beats[ max(beat['next'], first_beat_with_candidates) ]
+
+                    if beat == beats[-1]:
+                        beat = beats[ max(beat['next'], first_beat_with_candidates) ]
+                    else:
+                        beat = beats[beat['next']]
                 else:
                     beat = beats[ random.choice(non_recent_candidates) ]
 
