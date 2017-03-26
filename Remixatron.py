815194d5d418c0cd6aebcfe8dd2c19a518c090ac
Dave Rensin <dave@rensin.com>
2017-03-26 12:47:16 +0000

    dead code cleanup

--- Remixatron.py
+++ Remixatron.py
@@ -599,7 +599,7 @@ class InfiniteJukebox(object):
 
             self._clusters_list.append(entry)
 
-        # get the cluster with the segments/cluster ratio not great than 4. That
+        # get the max cluster with the segments/cluster ratio not great than 4. That
         # will produce the most musically pleasing effect
         max_seg_ratio = max( [cl['seg_ratio'] for cl in self._clusters_list] )
         max_seg_ratio = min( max_seg_ratio, 4 )
